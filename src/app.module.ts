bd7b09c5ebb8d752ef735b6d4522f2233e494ca4
selosele <osh3384@naver.com>
2022-10-01 23:34:23 +0900

    API 모듈 import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { CodeModule } from './code/code.module';
 import { ConfigModule } from './config/config.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
+import { PostsModule } from './posts/posts.module';
+import { YearModule } from './year/year.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), ConfigModule],
+  imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
+    CodeModule,
+    ConfigModule,
+    PostsModule,
+    YearModule,
+  ],
   controllers: [],
   providers: [],
 })
