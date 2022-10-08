1eaf6b1c1e98e1356c90254dfe77c10916aea1c7
selosele <osh3384@naver.com>
2022-10-08 23:31:50 +0900

    유효성 검사 추가

--- src/post/dto/search-post.dto.ts
+++ src/post/dto/search-post.dto.ts
@@ -1,9 +1,13 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class SearchPostDto {
 
   // 검색 옵션
+  @IsNotEmpty()
   t?: string;
 
   // 검색 키워드
+  @IsNotEmpty()
   q?: string;
 
   // 대소문자 구분 여부
