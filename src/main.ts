45d465366dfcdedb04b119a4a73302c4342de380
selosele <osh3384@naver.com>
2022-10-08 23:31:37 +0900

    유효성 검사 추가

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './utils/swagger/swagger.util';
@@ -5,8 +6,19 @@ import { setupSwagger } from './utils/swagger/swagger.util';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // whitelist 옵션은 검증을 통과한 뒤, 대상 객체에서 검증 규칙이 정의되어있지 않은 프로퍼티를 모두 제거해주는 옵션
+      whitelist: true,
+      // 대상 객체에 검증 규칙이 정의되어 있지 않은 프로퍼티가 있다면 오류를 내게 하는 옵션
+      forbidNonWhitelisted: false,
+      // auto-transformation을 가능하게 해주는 옵션
+      transform: true,
+    })
+  );
+
   setupSwagger(app);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
