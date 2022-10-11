1c6a3ffaef3248336f0fe80eba839b65c0562ecc
selosele <osh3384@naver.com>
2022-10-11 14:32:29 +0900

    csurf, cookie-parser 라이브러리 추가

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,18 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './utils/swagger/swagger.util';
+import * as cookieParser from 'cookie-parser';
+import * as csrf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(cookieParser());
+  app.use('/', csrf({ cookie: true }));
+
   app.useGlobalPipes(
     new ValidationPipe({
-      // whitelist 옵션은 검증을 통과한 뒤, 대상 객체에서 검증 규칙이 정의되어있지 않은 프로퍼티를 모두 제거해주는 옵션
+      // 검증을 통과한 뒤, 대상 객체에서 검증 규칙이 정의되어있지 않은 프로퍼티를 모두 제거해주는 옵션
       whitelist: true,
       // 대상 객체에 검증 규칙이 정의되어 있지 않은 프로퍼티가 있다면 오류를 내게 하는 옵션
       forbidNonWhitelisted: false,
