7311dd0f659cdbab7adf51b4686ad2705b56ef97
selosele <osh3384@naver.com>
2022-10-15 16:23:34 +0900

    JWT 환경변수 못 불러오는 문제 수정

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -10,6 +10,7 @@ import { CustomTypeOrmModule } from 'src/configs/CustomTypeOrmModule';
 import { UserRepository } from './user.repository';
 import { UserRoleRepository } from './user-role.repository';
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigService } from '@nestjs/config';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -17,11 +18,14 @@ dotenv.config();
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secret: process.env.JWT_SECRET_KEY,
-      signOptions: {
-        expiresIn: process.env.JWT_EXPIRATION_TIME,
-      }
+    JwtModule.registerAsync({
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET_KEY'),
+        signOptions: {
+          expiresIn: +config.get<number>('JWT_EXPIRATION_TIME'),
+        }
+      }),
+      inject: [ConfigService],
     }),
     TypeOrmModule.forFeature([User, UserRole]),
     CustomTypeOrmModule.forCustomRepository([UserRepository, UserRoleRepository]),
