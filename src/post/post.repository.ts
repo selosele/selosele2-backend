e3544bff35f9414e26228f63045a0633d1617060
selosele <osh3384@naver.com>
2022-11-03 23:24:01 +0900

    리팩토링 및 포스트 검색 작업중

--- src/post/post.repository.ts
+++ src/post/post.repository.ts
@@ -1,5 +1,9 @@
+import { Category } from 'src/category/category.entity';
+import { PostCategory } from 'src/category/post-category.entity';
 import { CustomRepository } from 'src/configs/CustomRepository';
 import { PaginationDto } from 'src/shared/dto/pagination.dto';
+import { PostTag } from 'src/tag/post-tag.entity';
+import { Tag } from 'src/tag/tag.entity';
 import { Repository } from 'typeorm';
 import { SearchPostDto } from './dto/search-post.dto';
 import { Post } from './post.entity';
@@ -10,6 +14,10 @@ export class PostRepository extends Repository<Post> {
   // 개수별 포스트 목록을 조회한다.
   async listPostByLimit(limit: number): Promise<Post[]> {
     return await this.find({
+      select: {
+        cont: false,
+        rawText: false,
+      },
       order: {
         regDate: 'DESC',
       },
@@ -19,26 +27,67 @@ export class PostRepository extends Repository<Post> {
 
   // 포스트를 검색한다.
   async listPostSearch(searchPostDto: SearchPostDto): Promise<Post[]> {
-    return await this.createQueryBuilder('post')
-      .select([
-        "id",
-        "title",
-        "reg_date AS regDate",
-        "SUBSTR(raw_text, 1, 180) AS rawText",
-        "og_img_url AS ogImgUrl",
-      ])
-      .where("title LIKE :title", { title: `%${searchPostDto.q}%` })
-      .orWhere("raw_text LIKE :raw_text", { raw_text: `%${searchPostDto.q}%` })
-      .orderBy("reg_date", "DESC")
-      .getRawMany();
+    let query = this.createQueryBuilder('post')
+      .select("post.id", "id")
+        .distinct(true)
+      .addSelect("post.title", "title")
+      .addSelect("post.reg_date", "regDate")
+      .addSelect("SUBSTR(post.raw_text, 1, 180)", "rawText")
+      .addSelect("post.og_img_url", "ogImgUrl")
+
+    // 전체 검색
+    if ('001' === searchPostDto.t) {
+      query = query
+        .leftJoin(PostCategory, "post_category", "post_category.post_id = post.id")
+        .leftJoin(Category, "category", "category.id = post_category.category_id")
+        .leftJoin(PostTag, "post_tag", "post_tag.post_id = post.id")
+        .leftJoin(Tag, "tag", "tag.id = post_tag.tag_id")
+        .where("post.title LIKE :title", { title: `%${searchPostDto.q}%` })
+        .orWhere("post.raw_text LIKE :raw_text", { raw_text: `%${searchPostDto.q}%` })
+        .orWhere("category.nm LIKE :nm", { nm: `%${searchPostDto.q}%` })
+        .orWhere("tag.nm LIKE :nm", { nm: `%${searchPostDto.q}%` })
+    }
+    
+    // 제목으로 검색
+    if ('002' === searchPostDto.t) {
+      query = query
+        .where("post.title LIKE :title", { title: `%${searchPostDto.q}%` })
+    }
+
+    // 내용으로 검색
+    if ('003' === searchPostDto.t) {
+      query = query
+        .where("post.raw_text LIKE :raw_text", { raw_text: `%${searchPostDto.q}%` })
+    }
+
+    // 카테고리로 검색
+    if ('004' === searchPostDto.t) {
+      query = query
+        .innerJoin(PostCategory, "post_category", "post_category.post_id = post.id")
+        .innerJoin(Category, "category", "category.id = post_category.category_id")
+        .where("category.nm LIKE :nm", { nm: `%${searchPostDto.q}%` })
+    }
+
+    // 태그로 검색
+    if ('005' === searchPostDto.t) {
+      query = query
+        .innerJoin(PostTag, "post_tag", "post_tag.post_id = post.id")
+        .innerJoin(Tag, "tag", "tag.id = post_tag.tag_id")
+        .where("tag.nm LIKE :nm", { nm: `%${searchPostDto.q}%` })
+    }
+
+    query = query
+      .orderBy("post.reg_date", "DESC");
+
+    return await query.getRawMany();
   }
 
   // 포스트의 연도 및 개수를 조회한다.
   async listYearAndCount(): Promise<Post[]> {
     return await this.createQueryBuilder('post')
-      .select("YEAR(reg_date) AS year")
+      .select("YEAR(reg_date)", "year")
         .distinct(true)
-      .addSelect("COUNT('year') AS count")
+      .addSelect("COUNT('year')", "count")
       .groupBy("year")
       .orderBy("year", "DESC")
       .getRawMany();
