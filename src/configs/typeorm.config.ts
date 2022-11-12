a471476f503b953b5337e6fb92000e931fef645b
selosele <osh3384@naver.com>
2022-11-12 19:39:10 +0900

    설정 변경

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -2,12 +2,12 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 export const typeORMConfig: TypeOrmModuleOptions = {
-  type: 'mariadb',
-  host: process.env.DB_HOST,
-  port: +process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATEBASE,
+  type: 'production' === process.env.NODE_ENV ? 'mysql' : 'mariadb',
+  host: 'production' === process.env.NODE_ENV ? process.env.DB_HOST_PROD : process.env.DB_HOST,
+  port: 'production' === process.env.NODE_ENV ? +process.env.DB_PORT_PROD : +process.env.DB_PORT,
+  username: 'production' === process.env.NODE_ENV ? process.env.DB_USERNAME_PROD : process.env.DB_USERNAME,
+  password: 'production' === process.env.NODE_ENV ? process.env.DB_PASSWORD_PROD : process.env.DB_PASSWORD,
+  database: 'production' === process.env.NODE_ENV ? process.env.DB_DATEBASE_PROD : process.env.DB_DATEBASE,
   entities: ['dist/**/*.entity.{js,ts}'],
   synchronize: false,
   namingStrategy: new SnakeNamingStrategy(),
