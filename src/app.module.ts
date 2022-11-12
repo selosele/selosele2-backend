c1f91732d8d3c930cfed35f49f8f3d9dffa91536
selosele <osh3384@naver.com>
2022-11-12 17:15:19 +0900

    프론트 빌드 관련 라이브러리 설치 및 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,11 +10,16 @@ import { WidgetModule } from './widget/widget.module';
 import { CategoryModule } from './category/category.module';
 import { TagModule } from './tag/tag.module';
 import { SatisfactionModule } from './satisfaction/satisfaction.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
     }),
     TypeOrmModule.forRoot(typeORMConfig),
     AuthModule,
