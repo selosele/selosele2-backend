e8724387f0fa00a2b9a81b20557e5e3d0299bbf1
selosele <osh3384@naver.com>
2022-12-24 13:16:51 +0900

    설정 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,7 +22,7 @@ import { BizExceptionFilter } from './shared/exception/biz-exception.filter';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      //ignoreEnvFile: 'production' === process.env.NODE_ENV,
+      ignoreEnvFile: 'production' === process.env.NODE_ENV,
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client'),
