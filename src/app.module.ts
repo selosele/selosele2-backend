0745624cecb50a3f734d4bfce89b7e97153756b6
selosele <osh3384@naver.com>
2022-12-24 13:21:48 +0900

    TEST PUSH

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,7 +22,7 @@ import { BizExceptionFilter } from './shared/exception/biz-exception.filter';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      ignoreEnvFile: 'production' === process.env.NODE_ENV,
+      ignoreEnvFile: true,
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client'),
