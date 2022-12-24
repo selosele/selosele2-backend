d45a9f4798b1cd05ec2b368cc3b51ef7b0698a92
selosele <osh3384@naver.com>
2022-12-24 13:47:15 +0900

    설정 변경

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -3,11 +3,11 @@ import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'mariadb',
-  host: 'production' === process.env.NODE_ENV ? process.env.DB_HOST_PROD : process.env.DB_HOST,
-  port: 'production' === process.env.NODE_ENV ? +process.env.DB_PORT_PROD : +process.env.DB_PORT,
-  username: 'production' === process.env.NODE_ENV ? process.env.DB_USERNAME_PROD : process.env.DB_USERNAME,
-  password: 'production' === process.env.NODE_ENV ? process.env.DB_PASSWORD_PROD : process.env.DB_PASSWORD,
-  database: 'production' === process.env.NODE_ENV ? process.env.DB_DATEBASE_PROD : process.env.DB_DATEBASE,
+  host: 'svc.gksl2.cloudtype.app',
+  port: 31382,
+  username: 'root',
+  password: 'll32glc1dk38c',
+  database: 'blog2',
   entities: ['dist/**/*.entity.{js,ts}'],
   synchronize: false,
   namingStrategy: new SnakeNamingStrategy(),
