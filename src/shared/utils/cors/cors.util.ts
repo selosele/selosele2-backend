e862420068ca4af8c417a2cff9b3e3c79e936b4f
selosele <osh3384@naver.com>
2023-01-01 15:48:31 +0900

    환경변수 추가

--- src/shared/utils/cors/cors.util.ts
+++ src/shared/utils/cors/cors.util.ts
@@ -3,10 +3,7 @@ import { INestApplication } from '@nestjs/common';
 /** Cors 유틸 */
 export const setupCors = (app: INestApplication): void => {
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://blog-selosele.koyeb.app'
-    ],
+    origin: process.env.LOC_ORIGIN,
     methods: 'GET,POST,PUT,DELETE', // 허용할 Request Method
     credentials: true,
   });
