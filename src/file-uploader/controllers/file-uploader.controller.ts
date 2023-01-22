af4f1bcc1370ccf6c164d8e69171d37561d31368
selosele <osh3384@naver.com>
2023-01-22 23:20:12 +0900

    공통 파일 업로드 API 작업

--- src/file-uploader/controllers/file-uploader.controller.ts
+++ src/file-uploader/controllers/file-uploader.controller.ts
@@ -1,7 +1,11 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, ParseFilePipe, Post, UploadedFiles, UseInterceptors } from "@nestjs/common";
 import { ApiCreatedResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { FileUploaderService } from "../services/file-uploader.service";
-import { FileUploaderResponse } from '../models/file-uploader.model';
+import { FileUploaderRequest, FileUploaderResponse } from '../models/file-uploader.model';
+import { Auth } from "src/shared/decorators";
+import { RoleEnum } from "src/auth/models";
+import { AnyFilesInterceptor } from "@nestjs/platform-express";
+import { FileTypeValidator, MaxFileSizeValidator } from "src/shared/utils";
 
 @Controller('file')
 @ApiTags('파일 업로더 API')
@@ -12,6 +16,7 @@ export class FileUploaderController {
   ) {}
 
   @Get()
+  @Auth(RoleEnum.ROLE_ADMIN)
   @ApiOperation({
     summary: '파일 목록 조회 API',
     description: '파일 목록을 조회한다.'
@@ -21,6 +26,26 @@ export class FileUploaderController {
   })
   listFile(): Promise<FileUploaderResponse[]> {
     return this.fileUploaderService.listFile();
+  }
+
+  @Post()
+  @Auth(RoleEnum.ROLE_ADMIN)
+  @UseInterceptors(AnyFilesInterceptor())
+  @ApiOperation({
+    summary: '파일 업로드 API',
+    description: '파일을 업로드한다.'
+  })
+  addFile(
+    @UploadedFiles(new ParseFilePipe({
+      validators: [
+        new MaxFileSizeValidator({ maxSize: 1000000 }),
+        new FileTypeValidator({ fileType: /(jpg|jpeg|png)$/ }),
+      ],
+    })) files: FileUploaderRequest[],
+  ): void {
+    for (let file of files) {
+      this.fileUploaderService.uploadImage(file);
+    }
   }
 
 }
