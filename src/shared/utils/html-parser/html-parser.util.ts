d892987149d813cb7db1b5de2d05742e02f17a10
selosele <osh3384@naver.com>
2023-09-28 11:55:41 +0900

    파라미터 없을 시 빈문자열 반환 처리

--- src/shared/utils/html-parser/html-parser.util.ts
+++ src/shared/utils/html-parser/html-parser.util.ts
@@ -1,7 +1,10 @@
 import { Options, parse as nodeHTMLParser } from 'node-html-parser';
+import { isBlank } from '../common/common.util';
 
 /** rawText로 파싱한 결과물을 반환한다. */
 export function getRawText(str: string): string {
+  if (isBlank(str)) return '';
+
   let opt: Options = {
     blockTextElements: {
       pre: false
