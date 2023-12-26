26cbc152e2fa1c641d84d583a8e5ab0be102f8ca
selosele <osh3384@naver.com>
2023-12-26 11:46:01 +0900

    환경변수 로드 개선

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,16 @@ import { NestExpressApplication } from '@nestjs/platform-express/interfaces/nest
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { setupCors, getLogLevels, setupSwagger, setupValidation } from './shared/utils';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    logger: getLogLevels(process.env.NODE_ENV), // 로그 레벨 설정
-  });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  /** 환경변수 service */
+  const config = app.get(ConfigService);
+
+  // 로그 레벨 설정
+  app.useLogger(getLogLevels(config.get<string>('NODE_ENV')));
 
   // API 호출 전역 접두사 설정
   app.setGlobalPrefix('api');
@@ -18,22 +23,23 @@ async function bootstrap() {
   // x-powered-by 헤더 삭제
   app.disable('x-powered-by');
 
-  if ('production' === process.env.NODE_ENV) {
+  if ('production' === config.get<string>('NODE_ENV')) {
     setupCors(app); // Cors 설정
   }
 
-  if ('development' === process.env.NODE_ENV) {
+  if ('development' === config.get<string>('NODE_ENV')) {
     setupSwagger(app); // Swagger 설정
   }
 
   // 유효성 검증 설정
   setupValidation(app);
 
-  const port = (process.env.PORT || 3000);
+  /** port */
+  const port = (config.get<number>('PORT') || 3000);
 
   await app.listen(port);
 
-  if ('development' === process.env.NODE_ENV) {
+  if ('development' === config.get<string>('NODE_ENV')) {
     console.log(`Server running at http://localhost:${port}..`);
   }
 }
