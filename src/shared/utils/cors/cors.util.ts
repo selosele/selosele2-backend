5aedab4f7c122cd362a71dad0a842c2660f2a286
selosele <osh3384@naver.com>
2024-01-11 14:55:12 +0900

    CORS 테스트

--- src/shared/utils/cors/cors.util.ts
+++ src/shared/utils/cors/cors.util.ts
@@ -12,8 +12,8 @@ export function setupCors(app: INestApplication, locOrigin: string): void {
         callback(new NotFoundException());
       }
     },
-    //allowedHeaders: ['Content-Type', 'Authorization'], // 허용할 Header
+    allowedHeaders: ['Content-Type', 'Authorization'], // 허용할 Header
     methods: 'GET,POST,PUT,DELETE', // 허용할 Request Method
-    credentials: true, // 요청에 인증 정보를 담아서 보낼지 여부
+    //credentials: true, // 요청에 인증 정보를 담아서 보낼지 여부
   });
 }
