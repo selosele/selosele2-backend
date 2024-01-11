b3ff996016d16d16d309c10371ee9fd8f6dabd9e
selosele <osh3384@naver.com>
2024-01-11 16:43:33 +0900

    CORS 테스트

--- src/shared/utils/cors/cors.util.ts
+++ src/shared/utils/cors/cors.util.ts
@@ -2,16 +2,17 @@ import { INestApplication, NotFoundException } from '@nestjs/common';
 
 /** Cors 유틸 */
 export function setupCors(app: INestApplication, locOrigin: string): void {
-  const whitelist = [locOrigin];
+  // const whitelist = [locOrigin];
 
   app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin || -1 !== whitelist.indexOf(origin)) {
-        callback(null, true);
-      } else {
-        callback(new NotFoundException());
-      }
-    },
+    // origin: (origin, callback) => {
+    //   if (!origin || -1 !== whitelist.indexOf(origin)) {
+    //     callback(null, true);
+    //   } else {
+    //     callback(new NotFoundException());
+    //   }
+    // },
+    origin: locOrigin,
     allowedHeaders: ['Content-Type', 'Authorization'], // 허용할 Header
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // 허용할 Request Method
     credentials: true, // 요청에 인증 정보를 담아서 보낼지 여부
