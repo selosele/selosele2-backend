8aea975fbd4091ea02858fd8155f4feb0fede18b
selosele <osh3384@naver.com>
2024-01-16 09:48:12 +0900

    isNotEmpty() 함수 import 수정

--- src/shared/utils/auth/auth.util.ts
+++ src/shared/utils/auth/auth.util.ts
@@ -1,6 +1,6 @@
 import { UserDto } from "@/auth/models";
 import { JwtService } from "@nestjs/jwt";
-import { isNotEmpty } from "class-validator";
+import { isNotEmpty } from "../common/common.util";
 
 /** 인증된 사용자 정보를 반환한다. */
 export function getAuthenticatedUser(token: string): UserDto {
