f408283778e2097dccb149bd987b1b9e3dc9a387
selosele <osh3384@naver.com>
2024-02-11 17:52:55 +0900

    리팩토링

--- src/shared/utils/auth/auth.util.ts
+++ src/shared/utils/auth/auth.util.ts
@@ -1,14 +1,10 @@
 import { UserDto } from "@/auth/models";
 import { JwtService } from "@nestjs/jwt";
-import { isNotEmpty } from "../common/common.util";
 
 /** 인증된 사용자 정보를 반환한다. */
 export function getAuthenticatedUser(token: string): UserDto {
   const jwtService = new JwtService();
   const user = jwtService.decode(token) as UserDto;
 
-  if (isNotEmpty(user)) {
-    return user;
-  }
-  return null;
+  return user ?? null;
 }
