df497c4b4e29d3f846c8027c1084a6ce1329ba90
selosele <osh3384@naver.com>
2024-03-09 20:44:13 +0900

    패키지 업데이트 원복

--- src/file-uploader/models/file-uploader.model.ts
+++ src/file-uploader/models/file-uploader.model.ts
@@ -1,9 +1,4 @@
 import { UploadApiErrorResponse, UploadApiResponse } from 'cloudinary';
-import { Express } from 'express';
-import { Multer } from 'multer';
-
-// 주의: Express, Multer interface를 사용하지 않더라도
-// import를 해야 네임스페이스 오류가 발생하지 않는다
 
 /** 파일 업로더 API */
 export interface FileUploaderApi {
