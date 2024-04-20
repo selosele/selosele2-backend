f652e1c03de916c25ede290215ac61a207d8ff30
selosele <osh3384@naver.com>
2024-04-20 17:07:54 +0900

    태그 검색 허용

--- src/index-search/models/codes/code.ts
+++ src/index-search/models/codes/code.ts
@@ -4,7 +4,7 @@ export const searchCodes = {
   SEARCH_TITLE:          { id: 'A01002', prefix: 'A01', val: '002', nm: '제목', desc: '포스트 검색조건', useYn: 'Y' },
   SEARCH_CONTENT:        { id: 'A01003', prefix: 'A01', val: '003', nm: '내용', desc: '포스트 검색조건', useYn: 'N' },
   SEARCH_CATEGORY:       { id: 'A01004', prefix: 'A01', val: '004', nm: '카테고리', desc: '포스트 검색조건', useYn: 'Y' },
-  SEARCH_TAG:            { id: 'A01005', prefix: 'A01', val: '005', nm: '태그', desc: '포스트 검색조건', useYn: 'N' },
+  SEARCH_TAG:            { id: 'A01005', prefix: 'A01', val: '005', nm: '태그', desc: '포스트 검색조건', useYn: 'Y' },
 
   INDEX_SEARCH_POST:     { id: 'D03001', prefix: 'D03', val: '001', nm: '포스트', desc: '검색 유형', useYn: 'Y' },
   INDEX_SEARCH_CONTENT:  { id: 'D03002', prefix: 'D03', val: '002', nm: '콘텐츠', desc: '검색 유형', useYn: 'Y' },
